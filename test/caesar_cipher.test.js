db3dfffbe2afbdd0b0dc11f8324e68d1657c36c4
IBUNHABIBU <ibunhabib@gmail.com>
2020-10-23 11:06:28 +0300

    testing caesar cipher

---
+++ test/caesar_cipher.test.js
@@ -0,0 +1,15 @@
+import caesarCipher from '../src/caesar_cipher';
+test('caesar cipher of a string wraping from z to a ', () => {
+  const result = caesarCipher('z',1);
+  expect(result).toBe('a');
+});
+
+test('caesar cipher of string with a mixed case', () => {
+  const result = caesarCipher('Kcfy oh Vcas',-14);
+  expect(result).toBe('Work at Home');
+});
+
+test('caesar cipher of string having punctuation', () => {
+  const result = caesarCipher('ABC!.abc', 2);
+  expect(result).toBe('CDE!.cde')
+})
