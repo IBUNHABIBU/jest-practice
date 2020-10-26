1c7214fa274e0355da5097de5edf1ac769a58baf
IBUNHABIBU <ibunhabib@gmail.com>
2020-10-26 12:25:58 +0300

    removing lint errors

--- src/caesar_cipher.js
+++ src/caesar_cipher.js
@@ -1,20 +1,20 @@
 const caesarCipher = (str, num) => {
-  num = num % 26;
-  var lowerCaseStr = str.toLowerCase();
-  var alpha = 'abcdefghijklmnopqrstuvwxyz';
-  var alphabet = alpha.split('');
-  var newStr = '';
-  for(var i = 0; i < lowerCaseStr.length; i++){
-    var currentLetter = lowerCaseStr[i];
-    if(currentLetter == ' ' || !alpha.includes(currentLetter)){
+  num %= 26;
+  const lowerCaseStr = str.toLowerCase();
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabet = alpha.split('');
+  const newStr = '';
+  for(let i = 0; i < lowerCaseStr.length; i++){
+    const currentLetter = lowerCaseStr[i];
+    if (currentLetter == ' ' || !alpha.includes(currentLetter)){
       newStr += currentLetter;
       continue;
     }
     var currentIndex = alphabet.indexOf(currentLetter);
     var newIndex = currentIndex + num; 
-    if(newIndex > 25) newIndex = newIndex - 26;
-    if(newIndex < 0) newIndex = newIndex + 26;
-    if(str[i] === str[i].toUpperCase()) {
+    if (newIndex > 25) newIndex = newIndex - 26;
+    if (newIndex < 0) newIndex = newIndex + 26;
+    if (str[i] === str[i].toUpperCase()) {
       newStr += alphabet[newIndex].toUpperCase();
     }
     else newStr += alphabet[newIndex];
